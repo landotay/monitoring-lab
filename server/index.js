0fedf4098a95b57d98081a3c29d90bb3862ff12c
Landon Taylor <landon.b.taylor@gmail.com>
2022-02-24 12:39:55 -0700

    added json parcer

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const rollbar = new Rollbar({
 rollbar.log('Hello world!')
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
